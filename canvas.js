618bc8acc8916dec4b378b190fffd8bbc85f7fe6
squidopia <squidopia0@gmail.com>
2026-02-13 10:41:07 +0100

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,8 +1,7 @@
 export const canvas = document.getElementById("game");
 export const ctx = canvas.getContext("2d");
 
-// Set canvas to full window size
-function resizeCanvas() {
+export function resizeCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 }
