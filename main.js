4046edc61954d0235cf7f5427e32a5a47144110e
squidopia <squidopia0@gmail.com>
2026-02-13 10:26:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+import { canvas, ctx } from './canvas.js';
 import { characters, activeCharacter, switchCharacter } from './characters.js';
 import { updateControls, moveCharacter } from './movement.js';
 import { drawLevel, drawCharacter } from './drawing.js';
 import { getCamera } from './camera.js';
 
+// --- MAIN LOOP ---
 function loop() {
   updateControls(activeCharacter);
   moveCharacter(activeCharacter);
 
   const camera = getCamera();
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawLevel(camera.x, camera.y);
 
   for (let key in characters) drawCharacter(characters[key], camera.x, camera.y);
