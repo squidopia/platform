db1bedebcc253a9ed857dcaee5e59e443c6dcd1d
squidopia <squidopia0@gmail.com>
2026-02-13 10:33:26 +0100

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,11 +1,13 @@
-import { activeCharacter } from './characters.js';
+import { gameState } from './characters.js';
+import { canvas } from './canvas.js';
 
 export function getCamera() {
   const lerp = 0.1;
-  if (!getCamera.x) getCamera.x = activeCharacter.x - window.innerWidth / 2;
-  if (!getCamera.y) getCamera.y = activeCharacter.y - window.innerHeight / 2;
+  if (!getCamera.x) getCamera.x = gameState.activeCharacter.x - canvas.width / 2;
+  if (!getCamera.y) getCamera.y = gameState.activeCharacter.y - canvas.height / 2;
 
-  getCamera.x += ((activeCharacter.x - window.innerWidth / 2) - getCamera.x) * lerp;
-  getCamera.y += ((activeCharacter.y - window.innerHeight / 2) - getCamera.y) * lerp;
+  getCamera.x += ((gameState.activeCharacter.x - canvas.width / 2) - getCamera.x) * lerp;
+  getCamera.y += ((gameState.activeCharacter.y - canvas.height / 2) - getCamera.y) * lerp;
+
   return { x: getCamera.x, y: getCamera.y };
 }
