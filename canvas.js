e119c51466a5815ec05a1097c4f999dd38152a9a
squidopia <squidopia0@gmail.com>
2026-02-13 10:26:39 +0100

    Create canvas.js

---
+++ canvas.js
@@ -0,0 +1,9 @@
+export const canvas = document.getElementById("game");
+export const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
